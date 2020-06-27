ff3792c7e2cd45fef8f35806cf2e8f6eee7caea4
Mofizur Rahman <moficodes@gmail.com>
2020-06-27 17:49:40 -0400

    error hanling

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let res = read_data_from_file().expect("some error");
+    println!("{}",res);
+
+
+}
+
+fn read_data_from_file() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
 }
