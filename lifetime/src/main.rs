1831a5c310c75dc7d2233ed90aa85235ae586ce7
Mofizur Rahman <moficodes@gmail.com>
2020-06-28 01:34:35 -0400

    lifetime

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,3 +1,48 @@
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // WONT COMPILE
+    // let r;
+    // {
+    //     let x = 5;
+    //     r = &x;
+    // }
+
+    // println!("{}", r);
+    
+    // WONT COMPILE
+    // let string1 = String::from("long string is long");
+    // let result;
+    // {
+    //     let string2 = String::from("xyz");
+    //     result = longest(string1.as_str(), string2.as_str());
+    // }
+    // println!("The longest string is {}", result);
+
+    // WORKS
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+    let novel = String::from("this is a novel idea. Hope the execution is good.");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    println!("{:?}", i);
+
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
